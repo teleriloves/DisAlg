19430581cf8c3112259a5980e2fb615511139818
virtualTeleri <teleriloves@gmail.com>
2017-10-24 18:45:12 +0200

    Comentarios, añadida fun de fact, sin acbar.

--- ROBLES_SORROCHE_ANTONIO/p1/DefenseStrategy.cpp
+++ ROBLES_SORROCHE_ANTONIO/p1/DefenseStrategy.cpp
@@ -21,21 +21,7 @@ using namespace Asedio;
 
 float cellValue(int row, int col, bool** freeCells, int nCellsWidth, int nCellsHeight
 	, float mapWidth, float mapHeight, List<Object*> obstacles, List<Defense*> defenses) {
-	return 0; // implemente aqui la funciÃ³n que asigna valores a las celdas
-
-	//primero implementar el resto, que funcione todito, y ya despues que funcione mejor o peor
-
-}
-
-//func fact: defensas, obstaculos, pos def, dim mapa, la def en si, list de def ya colocadas
-
-bool factibilityFunction(Vector3 pos, float mapWidth, float mapHeight, List<Object*> obstacles, Defense d, List<Defense*> defAlreadySetted)
-{
-
-	//Primero comprobar que la defensa no colisiona con los bordes
-	pos. 
-
-
+	return 0; // implemente aqui la función que asigna valores a las celdas
 }
 
 void DEF_LIB_EXPORTED placeDefenses(bool** freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight
